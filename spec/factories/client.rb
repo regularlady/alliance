671666ef67aee3f188e836cfe2304be4d598dd96
Brittany Martin <brittany.jill.martin@gmail.com>
2015-08-30 19:43:35 -0400

    Removed Salesforce ID from testing

--- spec/factories/client.rb
+++ spec/factories/client.rb
@@ -6,7 +6,6 @@ FactoryGirl.define do
     first_name    "Regina"
     last_name     "Phalange"
     phone         "+15005550006"
-    salesforce_id "1234567891234567"
     email         "hello@alliance.org"
   end
 end
